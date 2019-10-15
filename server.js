57e1cdc3e480e9674ebade6b26e48c5732439254
travis rodowick <travrodowick@yahoo.com>
2019-10-15 00:10:57 -0700

    changed port -- trying to troubleshoot heroku delpoyment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
